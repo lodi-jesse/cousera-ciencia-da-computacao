bb5533424dd0a66c3cbd38099f0b6ec2a3c0f196
lodi-jesse <everton.lodi@gmail.com>
2021-10-09 23:18:26 -0300

    soma hipotenusa

--- python/python_1/soma_hipotenusa.py
+++ python/python_1/soma_hipotenusa.py
@@ -1,5 +1,33 @@
-def soma_hipotenusa(n):
-    return print(n)
+import math
 
 
-soma_hipotenusa(n=int(input("Digite um número inteiro: ")))
+def é_hipotenusa(n):
+    i = 1
+    while i <= n:
+        j = 1
+        while j <= n:
+            a = i
+            b = j
+            c = int(math.sqrt((a ** 2) + (b ** 2)))
+            if (c > a) and (c > b) and (c < (a + b)):
+                if (a ** 2) + (b ** 2) == c ** 2:
+                    if c == n:
+                        return c
+            j = j + 1
+        i = i + 1
+
+    return 0
+
+
+def soma_hipotenusas(n):
+    soma = old = 0
+    for i in range(1, n + 1):
+        result = é_hipotenusa(i)
+        if result > old:
+            old = result
+            soma = soma + result
+
+    return print(soma)
+
+
+soma_hipotenusas(n=int(input("Digite um número: ")))
