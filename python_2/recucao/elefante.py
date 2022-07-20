1768055e81938cc87861291978bf5e2785344d92
everton <everton.lodi@gmail.com>
2022-07-20 10:48:44 -0300

    elefantes

---
+++ python_2/recucao/elefante.py
@@ -0,0 +1,14 @@
+def elefantes(n):
+    if n <= 0: return ""
+    if n == 1: return "Um elefante incomoda muita gente"
+    else:
+        return elefantes(n - 1) + '\n'+str(n) + ' elefantes ' + incomodam(n) +('muita mais') + '\n' + str(n) + ' elefantes incomodam muita gente'
+
+def incomodam(n):
+    if n <= 0: return ""
+    if n == 1: return "incomodam "
+    return "incomodam " + incomodam(n - 1)
+
+n = 4
+
+print(elefantes(n=n))
